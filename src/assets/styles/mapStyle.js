d81991787f12a20e9857cded9f0d0de81cca0d0e
Chris <chris.791.ix@gmail.com>
2019-07-27 19:29:07 -0700

    styled search bar and events tab (more styling needed)

---
+++ src/assets/styles/mapStyle.js
@@ -0,0 +1,157 @@
+export default [
+  {
+    "featureType": "administrative",
+    "stylers": [
+      {
+        "saturation": "-100"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.province",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "stylers": [
+      {
+        "saturation": -100
+      },
+      {
+        "lightness": 65
+      },
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "saturation": -100
+      },
+      {
+        "lightness": "50"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "stylers": [
+      {
+        "saturation": "-100"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "stylers": [
+      {
+        "lightness": "30"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "stylers": [
+      {
+        "visibility": "simplified"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "stylers": [
+      {
+        "lightness": "40"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "saturation": -100
+      },
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "hue": "white"
+      },
+      {
+        "saturation": 0
+      },
+      {
+        "lightness": -25
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "saturation": -100
+      },
+      {
+        "lightness": -25
+      }
+    ]
+  }
+]
---
+++ src/components/Map.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <gmap-map
+      ref       = "gmap"
+      :center   = "center"
+      :zoom     = "15"
+      :options  = "mapOptions"
+      :style    = "mapStyle"
+    >
+      <gmap-marker
+        v-for = "(m, index) in markers"
+
+        :key        = "index"
+        :position   = "m.position"
+        :icon       = "pin"
+        @click      = "center=m.position"
+      ></gmap-marker>
+    </gmap-map>
+
+    <PopularEventsWindow
+      v-if    = "loaded"
+      :events = "events"
+    />
+
+    <SearchBar
+      v-if    = "loaded"
+    />
+
+  </div>
+</template>
+
+<script>
+  // temporary event data
+  import events from '@/assets/eventData'
+
+  import mapStyle from '@/assets/styles/mapStyle'
+  import PopularEventsWindow from '@/components/ui/popularEventsWindow'
+  import SearchBar from '@/components/ui/searchBar'
+  import mapMarker from '@/assets/icons/pin.svg'
+
+  export default {
+    name: 'gmap',
+    components: {
+      PopularEventsWindow,
+      SearchBar
+    },
+    data() {
+      return {
+      // MAP SETTINGS (move to JSON?)
+        // Defaults Center to UBC
+        center:       { lat: 49.261, lng: -123.25 },
+        mapTypeId:    "terrain",
+        mapStyle:     "position: absolute; width:100%;  height:100%;",
+
+        mapOptions:   {
+          // CUSTOM FUNCTIONAL CONTROLS
+          disableDefaultUI:   true,
+          keyboardShortcuts:  false,
+          clickableIcons:     true,
+          // CUSTOM VIEW CONTROLS
+          minZoom: 14,
+          restriction: {
+            latLngBounds: {
+              north:  49.312836,
+              south:  49.198554,
+              west:   -123.298689,
+              east:   -123.094845
+            },
+            strictBounds: true,
+          },
+          // FURTHER DESIGN CUSTOMIZATIONS
+          styles: mapStyle,
+          draggableCursor:  '',
+          draggingCursor:    '',
+        },
+
+        // Boolean whether map is loaded
+        loaded: false,
+
+        // Google API Data
+
+        // markers take in an obj { lat: //, lng: //}
+        markers:      [
+          {
+            position: { lat: 49.261, lng: -123.25 }
+          }
+        ],
+        pin: {
+          url: mapMarker,
+          size: {width: 40, height: 40, f: 'px', b: 'px',},
+          scaledSize: {width: 40, height: 40, f: 'px', b: 'px',},
+        },
+
+
+        places:       [],
+        currentPlace: null,
+
+        events: events,
+      };
+    },
+
+
+    mounted() {
+      // Wait for google maps API to load
+      this.$gmapApiPromiseLazy().then(() => {
+        console.log("Map Loaded!");
+        this.loaded = true;
+        // Automatically try to determine user location on mount
+        // this.geolocate();
+      })
+    },
+
+    methods: {
+      geolocate: function() {
+        navigator.geolocation.getCurrentPosition(position => {
+          this.center = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+        });
+      }
+    }
+  };
+</script>
