245342056641e6ed784a2dfec34791fb793f0bbd
Chris <chris.791.ix@gmail.com>
2019-08-28 18:03:32 -0700

    added vue-mq

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 // Design Dependencies
+import VueMq from 'vue-mq'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -19,6 +20,16 @@ Vue.use( VueGoogleMaps, {
     libraries: "places"   // Allows autocomplete for "places" when searching
   }
 })
+
+// VueMq
+Vue.use(VueMq, {
+	breakpoints: {
+	  mobile: 600,
+	  // tablet: 900,
+	  // laptop: 1250,
+	  desktop: Infinity,
+	}
+  })
 
 // Load Vue Bootstrap
 Vue.use(BootstrapVue)
