7d01fcdf32df97f30310a1fd7b03f45330968fdc
Chris <chris.791.ix@gmail.com>
2019-10-13 20:16:13 -0700

    created MapboxStyle

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -1,5 +1,4 @@
 <!-- Landing page (using mapbox) -->
-
 <template>
   <MglMap
     container     = "mapbox-container"
@@ -14,6 +13,8 @@
 
 <script>
 
+  import MapboxStyle from "../assets/mapbox/style.json"
+
   import Mapbox from "mapbox-gl"
   import { MglMap, MglMarker } from "vue-mapbox"
 
@@ -27,7 +28,7 @@
       return {
       // MAP SETTINGS (move to JSON?)
         accessToken: "pk.eyJ1Ijoib25saW5rZXJzIiwiYSI6ImNrMWQyOHV6MDAzcnIzbm9laDdna213MWYifQ.-9uLdEhCUfIYP3ot-u5zeg",
-        mapStyle:     "mapbox://styles/mapbox/dark-v10",
+        mapStyle:     MapboxStyle,
         // https://docs.mapbox.com/help/glossary/style-url/
         // https://studio.mapbox.com/
         center:       [49.261, 0],
@@ -40,7 +41,7 @@
 
     methods: {
       onMapLoad(event) {
-        console.log("Map loaded!");
+        // console.log("Map loaded!");
         this.map = event.map;
         this.$store.map = event.map;
         this.$emit('mapLoad');
