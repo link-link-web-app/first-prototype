bbdd791c806e696cb384da256dfc4132d11a92ae
Chris <chris.791.ix@gmail.com>
2019-10-16 19:12:41 -0700

    refactor clean sass

--- src/assets/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const PUBLIC = 'PUBLIC';
-export const PRIVATE = 'PRIVATE';
-
-export const GOING = 'GOING';
-export const INTERESTED = 'INTERESTED';
-export const UNDECIDED = '';
-export const IGNORE = 'IGNORE';
