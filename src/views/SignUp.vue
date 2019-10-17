faada4c5f16b11e5bbd71f4d062c2f5c9fb1da1c
Chris <chris.791.ix@gmail.com>
2019-10-16 18:34:06 -0700

    refactor clean

--- src/views/SignUp.vue
+++
@@ -1,9 +0,0 @@
-// Login page allows user to Create a New Account
-// Allow Signing In through various external methods (google, facebook)
-// Allows redirection for "Forget Your Password" Page
-
-<template>
-  <div>
-    Welcome to the Sign Up Page!
-  </div>
-</template>
