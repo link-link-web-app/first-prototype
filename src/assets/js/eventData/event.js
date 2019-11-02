129e537fc6fe4a80880580244a6c5ff57c6e001c
Chris <chris.791.ix@gmail.com>
2019-11-01 18:42:11 -0700

    started working on backend event schema, restructured js

---
+++ src/assets/js/eventData/event.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// pointSchema
+const pointSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point",
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere",
+    required: true
+  }
+});
+
+// inviteeSchema
+const inviteeSchema = new Schema({
+  name: String,
+  status: String,
+  invited: Boolean
+});
+
+// imgSchema
+// will not work before body-parser implementation
+const imgSchema = new Schema({
+  img: {
+    data: Buffer,
+    contentType: String
+  }
+})
+
+// eventSettingsSchema
+const eventSettingsSchema = new Schema({
+  ticketURL: String,
+  media: {
+    coverImg: imgSchema,
+    hostImg: [imgSchema],
+    userImg: [imgSchema]
+  }
+})
+
+// create event Schema
+const eventSchema = new Schema({
+  private: {
+    // id: Number, // mongoDB automatically assigns id
+    adminId: Number,
+    location: {
+      type: pointSchema,
+      required: true
+    },
+    dateCreated: Date,
+    lastUpdated: Date,
+    type: String
+  },
+  public: {
+    name: String,
+    time: Date,
+    date: Date, // can possibly be merged with time
+    venue: String,
+    price: String,
+    host: String, // admin
+    description: String,
+    rating: Number // type: float
+  },
+  attendance: {
+    membersAccess: {}
+  }
+})
